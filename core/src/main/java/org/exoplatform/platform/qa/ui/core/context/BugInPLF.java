b54e08889ec314992ca53d958af016c534d9a61b
Maxime Gréau <mgreau@exoplatform.com>
2017-05-30 10:49:31 +0200

    Add BugInPLF annotation to tag disable tests due to bugs in eXo Platform

---
+++ core/src/main/java/org/exoplatform/platform/qa/ui/core/context/BugInPLF.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2003-2017 eXo Platform SAS.
+ *
+ * This file is part of eXo PLF:: QA - UI - API.
+ *
+ * eXo PLF:: QA - UI - API is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * eXo PLF:: QA - UI - API software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with eXo PLF:: QA - UI - API; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.platform.qa.ui.core.context;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * All tests marked by this annotation are disabled because of a bug in eXo Platform.
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Test
+@Disabled
+@Tag("BugInPLF")
+public @interface BugInPLF {
+
+  /**
+   * The <em>ID</em> of the JIRA that references the bug in eXo Platform.
+   */
+  String value();
+
+}
