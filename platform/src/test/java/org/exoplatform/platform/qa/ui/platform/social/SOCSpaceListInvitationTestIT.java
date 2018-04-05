fca8d4853fa478b2e4b1ecf1cc5cce6050d56176
ilyeskallala <ikallala@exoplatform.com>
2018-04-05 09:58:47 +0100

    Fix/sniff soc space list invitation (#132)
    * space invitation
    
    * add tags

---
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/social/SOCSpaceListInvitationTestIT.java
@@ -0,0 +1,104 @@
+package org.exoplatform.platform.qa.ui.platform.social;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
+import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomString;
+import static org.exoplatform.platform.qa.ui.selenium.locator.social.SocialLocator.ELEMENT_MY_SPACE_INVITATION_RECEIVED_ACCEPT_BTN;
+import static org.exoplatform.platform.qa.ui.selenium.locator.social.SocialLocator.ELEMENT_MY_SPACE_INVITATION_RECEIVED_CANCEL_BTN;
+import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.Condition;
+
+import org.exoplatform.platform.qa.ui.commons.Base;
+import org.exoplatform.platform.qa.ui.core.PLFData;
+import org.exoplatform.platform.qa.ui.selenium.platform.HomePagePlatform;
+import org.exoplatform.platform.qa.ui.selenium.platform.ManageLogInOut;
+import org.exoplatform.platform.qa.ui.selenium.platform.NavigationToolbar;
+import org.exoplatform.platform.qa.ui.selenium.platform.social.SpaceHomePage;
+import org.exoplatform.platform.qa.ui.selenium.platform.social.SpaceManagement;
+import org.exoplatform.platform.qa.ui.selenium.platform.social.SpaceSettingManagement;
+import org.exoplatform.platform.qa.ui.social.pageobject.AddUsers;
+
+@Tag("sniff")
+@Tag("social")
+public class SOCSpaceListInvitationTestIT extends Base {
+
+  NavigationToolbar      navigationToolbar;
+
+  AddUsers               addUsers;
+
+  ManageLogInOut         manageLogInOut;
+
+  SpaceManagement        spaceManagement;
+
+  SpaceHomePage          spaceHomePage;
+
+  SpaceSettingManagement spaceSettingManagement;
+
+  HomePagePlatform       homePagePlatform;
+
+  @BeforeEach
+  public void setupBeforeMethod() {
+    info("Start setUpBeforeMethod");
+    navigationToolbar = new NavigationToolbar(this);
+    addUsers = new AddUsers(this);
+    manageLogInOut = new ManageLogInOut(this);
+    spaceManagement = new SpaceManagement(this);
+    spaceHomePage = new SpaceHomePage(this);
+    spaceSettingManagement = new SpaceSettingManagement(this);
+    homePagePlatform = new HomePagePlatform(this);
+    manageLogInOut.signInCas(PLFData.DATA_USER1, "gtngtn");
+  }
+
+  /**
+   * <li>Case ID:121892.</li>
+   * <li>Test Case Name: Check Invitation space list.</li>
+   * <li>Pre-Condition:</li>
+   * <li>Post-Condition:</li> Step Number: 1 Step Name: Step 1: Check displaying
+   * on Invitations Received space list Step Description: - Create new space -
+   * Send Invitation to user - Log in by invited user - Go to My space page and
+   * Invitations Received tab Input Data: Expected Outcome: - Show all invited
+   * space. User can accept/ignore the invitation
+   */
+  @Test
+  public void test01_CheckInvitaionSpaceList() {
+    info("Test 01: Check Invitation space list");
+    String space = "space" + getRandomNumber();
+    String username1 = "usernamea" + getRandomString();
+    String email1 = username1 + "@test.com";
+    String username2 = "usernameb" + getRandomString();
+    String email2 = username2 + "@test.com";
+    String password = "123456";
+    info("Add user");
+    navigationToolbar.goToAddUser();
+    addUsers.addUser(username1, password, email1, username1, username1);
+    addUsers.addUser(username2, password, email2, username2, username2);
+    manageLogInOut.signIn(username1, password);
+
+    info("Create a space");
+    homePagePlatform.goToMySpaces();
+    spaceManagement.addNewSpaceSimple(space, space);
+
+    spaceHomePage.goToSpaceSettingTab();
+    spaceSettingManagement.inviteUser(username2, false, "");
+
+    manageLogInOut.signIn(username2, password);
+
+    homePagePlatform.goToMySpaces();
+    spaceManagement.goToInvitationsReceivedTab();
+    info("Verify that invitation is shown in the list with accept and ignore button");
+    $(byXpath(ELEMENT_MY_SPACE_INVITATION_RECEIVED_ACCEPT_BTN.replace("${space}", space))).should(Condition.exist);
+    $(byXpath(ELEMENT_MY_SPACE_INVITATION_RECEIVED_CANCEL_BTN.replace("${space}", space))).should(Condition.exist);
+    manageLogInOut.signIn(PLFData.DATA_USER1, "gtngtn");
+    navigationToolbar.goToManageCommunity();
+    addUsers.deleteUser(username1);
+    addUsers.deleteUser(username2);
+
+  }
+
+}
