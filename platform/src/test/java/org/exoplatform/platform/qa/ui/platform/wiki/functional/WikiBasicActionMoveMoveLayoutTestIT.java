beac14a5fdcbd9b38ef591ec790676716bf5a835
bsallem <bsallem@exoplatform.com>
2018-06-04 11:33:17 +0100

    WikiBasicActionMoveMoveLayoutTestIT

--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/wiki/functional/WikiBasicActionMoveMoveLayoutTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/wiki/functional/WikiBasicActionMoveMoveLayoutTestIT.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.$;
 import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
+import static org.exoplatform.platform.qa.ui.selenium.locator.wiki.WikiLocators.ELEMENT_MOVE_PAGE_DESTINATION_LABEL;
 import static org.exoplatform.platform.qa.ui.selenium.locator.wiki.WikiLocators.ELEMENT_MOVE_PAGE_SELECT_THE_DESTINATION_LABEL;
 import static org.exoplatform.platform.qa.ui.selenium.locator.wiki.WikiLocators.ELEMENT_MOVE_SPACESWITCHER;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
@@ -147,4 +148,61 @@ public class WikiBasicActionMoveMoveLayoutTestIT extends Base {
         info("Space switcher is displayed");
         $(ELEMENT_MOVE_SPACESWITCHER).waitUntil(Condition.visible,Configuration.timeout);
 
+    }
+
+    @Test
+    public  void test03_MoveLayoutIsDisplayingSelectTheDestinationLabels(){
+        String space1 = "space1"+ getRandomNumber();
+        String title = "title" +getRandomNumber();
+        String content =  "content"+ getRandomNumber();
+        info("Create data test");
+        info("Create Space 1 with a wiki page 1");
+        homePagePlatform.goToMySpaces();
+        spaceManagement.addNewSpaceSimple(space1, space1);
+         /*Step Number: 1
+         *Step Name: Step 1
+         *Step Description:
+         *- Go to the wiki of the "Space Move"
+         *Input Data:
+         *Expected Outcome:
+         *- Wiki is displayed*/
+         /*Step Number: 2
+         *Step Name: Step 2
+         *Step Description:
+         *- Create a new Page with the title Page A
+         *- Save
+         *Input Data:
+         *Expected Outcome:
+         *- Page A is displayed*/
+        info("Create wiki page in sapce1");
+        spaceHomePage.goToWikiTab();
+        wikiHomePage.goToAddBlankPage();
+        richEditor.addSimplePage(title, content);
+        wikiManagement.saveAddPage();
+        /*Step Number: 3
+        *Step Name: Step 3
+        *Step Description:
+        *- Click on [More] on menu
+        *Input Data:
+        *Expected Outcome:
+        *- Move option is available*/
+        wikiHomePage.goToMovePageForm();
+        /*Step Number: 5
+         *Step Name: Step 5
+         *Step Description:
+         *- Click on Move options
+         *Input Data:
+         *Expected Outcome:
+         *- A popup is displayed with the space switcher*/
+         /*Step Number: 6
+         *Step Name: Step 6
+         *Step Description:
+         *- Click on Move options
+         *Input Data:
+         *Expected Outcome:
+         *- A popup is displayed to move the page
+         *- Label "Destination:" is displayed outside and above the destination container.*/
+        info("Test 3: Move layout should display destination labels outside destination container");
+        info("Label Destination is displayed outside and above the destination container");
+        $(ELEMENT_MOVE_PAGE_DESTINATION_LABEL).waitUntil(Condition.visible,Configuration.timeout);
     }}
