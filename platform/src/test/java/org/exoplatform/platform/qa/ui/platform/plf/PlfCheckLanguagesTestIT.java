bef971f08d40286f2912c30af62a40fbbb30da4f
Aymen Boughdiri <aboughdiri@exoplatform.com>
2018-10-02 11:30:44 +0100

    add check basic language activity stream portuguse and spanch (#208)

--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/plf/PlfCheckLanguagesTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/plf/PlfCheckLanguagesTestIT.java
@@ -1,10 +1,17 @@
 package org.exoplatform.platform.qa.ui.platform.plf;
 
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byText;
-import static org.exoplatform.platform.qa.ui.selenium.locator.HomePageLocator.ELEMENT_BUTTON_CANCEL_CHANGE_LANGUAGES;
-import static org.exoplatform.platform.qa.ui.selenium.locator.HomePageLocator.ELEMENT_POPUP_LIST_OF_LANGUAGES;
+import static com.codeborne.selenide.Selenide.$;
+import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
+import static org.exoplatform.platform.qa.ui.selenium.locator.HomePageLocator.*;
+import static org.exoplatform.platform.qa.ui.selenium.locator.HomePageLocator.ELEMENT_DELETE_ACTIVITY;
+import static org.exoplatform.platform.qa.ui.selenium.locator.HomePageLocator.ELEMENT_DELETE_POPUP_OK;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
 
+import org.exoplatform.platform.qa.ui.selenium.platform.ActivityStream;
+import org.exoplatform.platform.qa.ui.selenium.platform.administration.ChangeLanguages;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -26,12 +33,18 @@ public class PlfCheckLanguagesTestIT extends Base {
 
   ManageLogInOut    manageLogInOut;
 
+  ChangeLanguages changeLanguages;
+
+  ActivityStream activityStream;
+
   @BeforeEach
   public void setupBeforeMethod() {
     info("Start setUpBeforeMethod");
     manageLogInOut = new ManageLogInOut(this);
     navigationToolbar = new NavigationToolbar(this);
     manageLogInOut.signInCas(PLFData.DATA_USER1, "gtngtn");
+    changeLanguages= new ChangeLanguages (this);
+    activityStream=new ActivityStream (this);
   }
 
   @Test
@@ -61,5 +74,33 @@ public class PlfCheckLanguagesTestIT extends Base {
     ELEMENT_POPUP_LIST_OF_LANGUAGES.find(byText("Ukrainian")).shouldBe(Condition.visible);
     ELEMENT_POPUP_LIST_OF_LANGUAGES.find(byText("Simplified Chinese")).shouldBe(Condition.visible);
     ELEMENT_BUTTON_CANCEL_CHANGE_LANGUAGES.click();
+  }
+
+  @Test
+  public void test02_addActivity_on_spanichlanguage() {
+    info("Test 1: add activity on spanich language");
+    // change language
+    navigationToolbar.goToChangeLanguage();
+    changeLanguages.changeLanguage("Spanish - Spain","Apply");
+    String activity1 = "activity1" + getRandomNumber();
+    activityStream.addActivity(activity1, "");
+    activityStream.deleteactivity(activity1);
+    info("the activity is removed successfully");
+    navigationToolbar.goToChangeLanguage();
+    changeLanguages.changeLanguage("Inglés","Aplicar");
+  }
+
+  @Test
+  public void test03_addActivity_on_Portugueselanguage() {
+    info("Test 1: add activity on Portuguese - Brazil language");
+    // change language
+    navigationToolbar.goToChangeLanguage();
+    changeLanguages.changeLanguage("Portuguese - Brazil","Apply");
+    String activity1 = "activity1" + getRandomNumber();
+    activityStream.addActivity(activity1, "");
+    activityStream.deleteactivity(activity1);
+    info("the activity is removed successfully");
+    navigationToolbar.goToChangeLanguage();
+    changeLanguages.changeLanguage("Inglês","Aplicar");
   }
 }
