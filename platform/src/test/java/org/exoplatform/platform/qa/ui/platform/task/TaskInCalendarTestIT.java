cdc3bb5de6133c78d057fa010661dc1114effad9
esmaoui <esmaoui@exoplatform.com>
2018-10-05 11:56:18 +0100

    verify last comments

--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/task/TaskInCalendarTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/task/TaskInCalendarTestIT.java
@@ -1,14 +1,10 @@
 package org.exoplatform.platform.qa.ui.platform.task;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.commands.Click;
 import org.exoplatform.platform.qa.ui.calendar.pageobject.CalendarHomePage;
 import org.exoplatform.platform.qa.ui.calendar.pageobject.CalendarManagement;
-import org.exoplatform.platform.qa.ui.calendar.pageobject.TaskManagement;
 import org.exoplatform.platform.qa.ui.commons.Base;
 import org.exoplatform.platform.qa.ui.core.PLFData;
-import org.exoplatform.platform.qa.ui.core.context.BugInPLF;
 import org.exoplatform.platform.qa.ui.selenium.platform.HomePagePlatform;
 import org.exoplatform.platform.qa.ui.selenium.platform.ManageLogInOut;
 import org.exoplatform.platform.qa.ui.selenium.platform.social.SpaceHomePage;
@@ -22,31 +18,25 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
+import static org.exoplatform.platform.qa.ui.selenium.locator.calender.CalendarLocator.ELEMENT_TASK_DETAIL;
 import static org.exoplatform.platform.qa.ui.selenium.locator.taskmanagement.TaskManagementLocator.*;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by esmaoui on 02/03/2018.
  */
+
 @Tag("sniff")
 @Tag("task")
-public class TaskInCalendar extends Base {
+public class TaskInCalendarTestIT extends Base {
 
     HomePagePlatform homePagePlatform;
-
     SpaceManagement spaceManagement;
-
     SpaceHomePage spaceHomePage;
-
     TasksManagement tasksManagement;
-    TaskManagement taskManagement;
-
     ProjectsManagement projectsManagement;
     CalendarManagement calendarManagement;
     CalendarHomePage calendarHomePage;
-
-
     ManageLogInOut manageLogInOut;
 
     @BeforeEach
@@ -62,9 +52,7 @@ public class TaskInCalendar extends Base {
         calendarHomePage = new CalendarHomePage(this);
         manageLogInOut = new ManageLogInOut(this);
         manageLogInOut.signInCas(PLFData.USER_ROOT, PLFData.password);
-
     }
-
     @Test
     @Tag("TA-605")
     public void test01_AddTaskInSpace() throws InterruptedException {
@@ -72,7 +60,6 @@ public class TaskInCalendar extends Base {
         String space = "space" + getRandomNumber();
         String taskName = "task" + getRandomNumber();
         String ContentTask = "contentTask" + getRandomNumber();
-
 
         info("Create a space");
         homePagePlatform.goToAllSpace();
@@ -83,20 +70,18 @@ public class TaskInCalendar extends Base {
         tasksManagement.addTask(taskName);
         ELEMENT_WORK_PLANED_FIELD.click();
         if (ELEMENT_CHECKBOX_ALL_DAY.is(Condition.selected)) {
-
             ElementsCollection elements = $$(byClassName("today"));
             elements.get(0).click();
             elements.get(1).click();
         }
-            ELEMENT_WORK_PLANED_FIELD.click();
-            ELEMENT_SHOW_IN_CALENDAR.parent().find(byClassName("uiIconPLFCalendar")).click();
-            projectsManagement.editProject(space, space, space, "", true);
-            spaceManagement.goToAgendaTab();
-            calendarHomePage.goToView(CalendarHomePage.selectViewOption.LIST);
-             $(byClassName("titleList")).parent().parent().parent().find(byText(taskName))
-                     .shouldBe(Condition.exist);
-             homePagePlatform.goToMySpaces();
-            spaceManagement.deleteSpace(space,false);
+        ELEMENT_WORK_PLANED_FIELD.click();
+        ELEMENT_SHOW_IN_CALENDAR.parent().find(byClassName("uiIconPLFCalendar")).click();
+        projectsManagement.editProject(space, space, space, "", true);
+        spaceManagement.goToAgendaTab();
+        calendarHomePage.goToView(CalendarHomePage.selectViewOption.LIST);
+        ELEMENT_TASK_DETAIL.parent().parent().parent().find(byText(taskName))
+                .shouldBe(Condition.exist);
+        homePagePlatform.goToMySpaces();
+        spaceManagement.deleteSpace(space,false);
         }
-
     }
