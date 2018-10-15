ecb96eacdb44bcb23da7481e2ac139c87006b434
esmaoui <esmaoui@exoplatform.com>
2018-10-15 09:58:40 +0100

    Add event in chat room

--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/chat/ChatCollaborativeActionsTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/chat/ChatCollaborativeActionsTestIT.java
@@ -2,13 +2,13 @@ package org.exoplatform.platform.qa.ui.platform.chat;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.ElementsCollection;
 import org.exoplatform.platform.qa.ui.calendar.pageobject.CalendarHomePage;
 import org.exoplatform.platform.qa.ui.calendar.pageobject.EventManagement;
 import org.exoplatform.platform.qa.ui.chat.pageobject.RoomManagement;
 import org.exoplatform.platform.qa.ui.commons.Base;
 import org.exoplatform.platform.qa.ui.core.PLFData;
 import org.exoplatform.platform.qa.ui.gatein.pageobject.UserAddManagement;
+import org.exoplatform.platform.qa.ui.gatein.pageobject.UserAndGroupManagement;
 import org.exoplatform.platform.qa.ui.selenium.platform.HomePagePlatform;
 import org.exoplatform.platform.qa.ui.selenium.platform.ManageLogInOut;
 import org.exoplatform.platform.qa.ui.selenium.platform.NavigationToolbar;
@@ -22,54 +22,52 @@ import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
 import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomString;
 import static org.exoplatform.platform.qa.ui.selenium.locator.chat.ChatLocator.*;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
+
 @Tag("chat")
 @Tag("sniff")
 
 public class ChatCollaborativeActionsTestIT extends Base {
 
-
-    HomePagePlatform     homePagePlatform;
-    ManageLogInOut       manageLogInOut;
-    NavigationToolbar    navigationToolbar;
-    RoomManagement       roomManagement;
-    CalendarHomePage     calendarHomePage;
-    EventManagement      eventManagement;
-    UserAddManagement    userAddManagement;
-
-
+    HomePagePlatform homePagePlatform;
+    ManageLogInOut manageLogInOut;
+    NavigationToolbar navigationToolbar;
+    RoomManagement roomManagement;
+    CalendarHomePage calendarHomePage;
+    EventManagement eventManagement;
+    UserAddManagement userAddManagement;
+    UserAndGroupManagement userAndGroupManagement;
 
     @BeforeEach
     public void setupBeforeMethod() {
         info("Start setUpBeforeMethod");
         homePagePlatform = new HomePagePlatform(this);
-        manageLogInOut= new ManageLogInOut(this);
-        navigationToolbar =new NavigationToolbar(this);
+        manageLogInOut = new ManageLogInOut(this);
+        navigationToolbar = new NavigationToolbar(this);
         roomManagement = new RoomManagement(this);
-        calendarHomePage= new CalendarHomePage(this);
-        eventManagement= new EventManagement(this);
-        userAddManagement= new UserAddManagement(this);
+        calendarHomePage = new CalendarHomePage(this);
+        eventManagement = new EventManagement(this);
+        userAddManagement = new UserAddManagement(this);
+        userAndGroupManagement=new UserAndGroupManagement(this);
         manageLogInOut.signInCas(PLFData.DATA_USER1, PLFData.DATA_PASS2);
-
     }
+
     @Test
     public void test01_AddEventInChatRoom() {
 
-        String room = "room" +    getRandomNumber();
-        String username= "username" + getRandomNumber();
-        String event= "event" + getRandomNumber();
-        String location= "location" + getRandomNumber();
-
-        String password= "123456";
-        String FirstName= "FirstName"+getRandomString();
-        String LastName= "LastName" + getRandomString();
-        String email= username + "@test.com";
-
+        String room = "room" + getRandomNumber();
+        String username = "username" + getRandomNumber();
+        String event = "event" + getRandomNumber();
+        String location = "location" + getRandomNumber();
+        String password = "123456";
+        String FirstName = "FirstName" + getRandomString();
+        String LastName = "LastName" + getRandomString();
+        String email = username + "@test.com";
 
         navigationToolbar.goToAddUser();
-        userAddManagement.addUser(username,password,email,FirstName,LastName);
+        userAddManagement.addUser(username, password, email, FirstName, LastName);
         homePagePlatform.goToChat();
         switchTo().window(1);
-        roomManagement.addRoom(room,username);
+        roomManagement.addRoom(room, username);
         ELEMENT_CONTACT_LIST.find(byText(room)).click();
         ELEMENT_COLLABORATION_ACTIONS.click();
         ELEMENT_CHAT_ADD_EVENT.click();
@@ -80,15 +78,18 @@ public class ChatCollaborativeActionsTestIT extends Base {
         ELEMENT_CHAT_EVENT_TO_DATE_.click();
         $(byClassName("today")).click();
         ELEMENT_CHAT_EVENT_LOCATION.setValue(location);
-        $(byXpath("//*[@id=\"appComposerForm\"]/div[2]/button")).click();
-        ELEMENT_CONTAINER_LIST_MESSAGES.find(byText(event)).waitUntil(Condition.appear,Configuration.timeout);
+        ELEMENT_CHAT_POST_EVENT.click();
+        ELEMENT_CONTAINER_LIST_MESSAGES.find(byText(event)).waitUntil(Condition.appear, Configuration.timeout);
         switchToParentWindow();
         manageLogInOut.signOut();
-        manageLogInOut.signInCas(username,password);
-        $(byClassName("uiIconStatus")).click();
+        manageLogInOut.signInCas(username, password);
+        ELEMENT_CHAT_ICON_STATUS.click();
         ELEMENT_CHAT_NOTIFICATION.find(byText(event)).should(Condition.appears);
         homePagePlatform.goToCalendarPage();
-
+        $(byText(event)).waitUntil(Condition.appear,Configuration.timeout);
+        manageLogInOut.signOut();
+        manageLogInOut.signInCas(PLFData.DATA_USER1,PLFData.DATA_PASS2);
+        navigationToolbar.goToManageCommunity();
+        userAndGroupManagement.deleteUser(username);
     }
-
 }
--- selenium/src/main/java/org/exoplatform/platform/qa/ui/selenium/locator/chat/ChatLocator.java
+++ selenium/src/main/java/org/exoplatform/platform/qa/ui/selenium/locator/chat/ChatLocator.java
@@ -117,7 +117,7 @@ public class ChatLocator {
 
     public static final SelenideElement ELEMENT_CONTAINER_LIST_MESSAGES = $(byId("chats"));
     public static final SelenideElement ELEMENT_CHAT_NOTIFICATION=   $(byClassName("dropdown-menu")).find(byXpath("//*[@id=\"chat-notifications-details\"]/a"));
-
+    public static final SelenideElement ELEMENT_CHAT_ICON_STATUS=   $(byClassName("uiIconStatus"));
     public static final SelenideElement ELEMENT_CHAT_CONTACT = $(byClassName("chat-contact"));
     public static final SelenideElement ELEMENT_DELETE_ROOM = $(byClassName("room-setting-action-deleteRoom"));
     public static final SelenideElement ELEMENT_CONFIRM_BUTTON_DELETE_ROOM = $(byId("team-delete-button-ok"));
@@ -128,8 +128,5 @@ public class ChatLocator {
     public static final SelenideElement ELEMENT_CHAT_EVENT_FROM_DATE_= $(byXpath("//*[@id=\"appComposerForm\"]/div[1]/div/div[1]/input"));
     public static final SelenideElement ELEMENT_CHAT_EVENT_TO_DATE_= $(byXpath("//*[@id=\"appComposerForm\"]/div[1]/div/div[2]/input"));
     public static final SelenideElement ELEMENT_CHAT_EVENT_LOCATION =$(byXpath("//*[@id=\"appComposerForm\"]/div[1]/input[2]"));
-
-
-
-
+    public static final SelenideElement ELEMENT_CHAT_POST_EVENT =$(byXpath("//*[@id=\"appComposerForm\"]/div[2]/button"));
 }
