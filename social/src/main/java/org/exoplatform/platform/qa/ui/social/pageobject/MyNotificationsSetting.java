5d0e3ab728d15383fd8d97ebc49c88e730b0fbf7
bsallem <bsallem@exoplatform.com>
2018-11-29 15:08:21 +0100

    SOCNotificationIntranetNotificationIconTestIT

--- social/src/main/java/org/exoplatform/platform/qa/ui/social/pageobject/MyNotificationsSetting.java
+++ social/src/main/java/org/exoplatform/platform/qa/ui/social/pageobject/MyNotificationsSetting.java
@@ -1,7 +1,6 @@
 package org.exoplatform.platform.qa.ui.social.pageobject;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.refresh;
 import static org.exoplatform.platform.qa.ui.selenium.locator.NavigationToolBarLocator.ELEMENT_ADD_TOOTLBAR;
 import static org.exoplatform.platform.qa.ui.selenium.locator.social.SocialLocator.*;
@@ -10,7 +9,6 @@ import static org.exoplatform.platform.qa.ui.selenium.testbase.LocatorTestBase.E
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.commands.ScrollTo;
 import org.exoplatform.platform.qa.ui.selenium.TestBase;
 import org.exoplatform.platform.qa.ui.selenium.testbase.ElementEventTestBase;
 
@@ -21,7 +19,7 @@ public class MyNotificationsSetting {
 
   /**
    * constructor
-   * 
+   *
    * @param testBase TestBase
    */
   public MyNotificationsSetting(TestBase testBase) {
@@ -294,8 +292,6 @@ public class MyNotificationsSetting {
         if($(ELEMENT_EDIT_NEWUSER_WEB_CHECKBOX).is(Condition.not(Condition.checked)))
           $(ELEMENT_EDIT_NEWUSER_WEB_CHECKBOX).parent().click();
         info("Click on Save button");
-        // scroll up
-        executeJavaScript("window.scrollBy(0,-2000)", "");
         $(ELEMENT_EDIT_NEWUSER_SAVE_BTN).click();
         $(ELEMENT_EDIT_NEWUSER_SAVE_BTN).waitUntil(Condition.not(Condition.visible),Configuration.timeout);
         info("Verify that intranet notification is shown");
@@ -567,7 +563,7 @@ public class MyNotificationsSetting {
       }
     } else {
       if (evt.isElementPresent(ELEMENT_SWITCH_ONOFF_WEB_ON)) {
-       $(ELEMENT_SWITCH_ONOFF_WEB_ON).parent().click();
+        $(ELEMENT_SWITCH_ONOFF_WEB_ON).parent().click();
       } else
         info("Intranet notification is already OFF");
 
