eeaf3af3ca7733aafb44bb7363e7492c93e565d2
kriahi12 <kriahi@exoplatform.com>
2020-05-14 03:36:39 +0200

    Rewrite Test Cases For Ecms Sniff

--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/ecms/EcmsSEInfoTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/ecms/EcmsSEInfoTestIT.java
@@ -1,36 +1,27 @@
 package org.exoplatform.platform.qa.ui.platform.ecms;
-
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 import static org.exoplatform.platform.qa.ui.core.PLFData.*;
 import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-
 import org.exoplatform.platform.qa.ui.commons.Base;
 import org.exoplatform.platform.qa.ui.ecms.pageobject.SiteExplorerHome;
 import org.exoplatform.platform.qa.ui.selenium.platform.ManageLogInOut;
 import org.exoplatform.platform.qa.ui.selenium.platform.NavigationToolbar;
 import org.exoplatform.platform.qa.ui.selenium.platform.ecms.ECMS_Permission;
-
 @Tag("ecms")
 @Tag("sniff")
 public class EcmsSEInfoTestIT extends Base {
   NavigationToolbar navigationToolbar;
-
   SiteExplorerHome  siteExplorerHome;
-
   ECMS_Permission   ecms_permission;
-
   ManageLogInOut    manageLogInOut;
-
   @BeforeEach
   public void setupBeforeMethod() {
     info("Start setUpBeforeMethod");
@@ -40,32 +31,13 @@ public class EcmsSEInfoTestIT extends Base {
     manageLogInOut = new ManageLogInOut(this);
     manageLogInOut.signInCas(DATA_USER1, "gtngtn");
   }
-
-  /**
-   * <li>Case ID:116591.</li>
-   * <li>Test Case Name: Add Permission.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li> *
-   * <li>Case ID:116639.</li>
-   * <li>Test Case Name: Edit Permission.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li> *
-   * <li>Case ID:116640.</li>
-   * <li>Test Case Name: Delete Permission.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li> Step Number: 1 Step Name: - Step Description: Step
-   * 1: Add Permission Input Data: - Select one node - Click on Permissions icon
-   * on action bar - Add permission for node - Choose user/group to add permission
-   * - Click Save - Log out and log in with an user who match with the permission
-   * set above Expected Outcome: - The node is added permission - User will take
-   * action on it as his rights
-   */
   @Test
   @Tag("eabis")
-  public void test01_AddPermission() {
-    info("Test 1: Add Permission");
+  public void test_01_AddEditDelete_PermissionThenViewMetadata() {
+    info("Add Permission");
     info("Get data test");
     String node1 = "node1" + getRandomNumber();
+    String node2 = "node2" + getRandomNumber();
     String folderType = "Content Folder";
     info("Finished getting data test");
     navigationToolbar.goToSiteExplorer();
@@ -86,113 +58,24 @@ public class EcmsSEInfoTestIT extends Base {
     // Delete data
     manageLogInOut.signIn(DATA_USER1, "gtngtn");
     navigationToolbar.goToSiteExplorer();
-    info("Test 02: Edit Permission");
-    siteExplorerHome.selectNode(node1);
-    siteExplorerHome.goToPermission();
-    ecms_permission.changeRight("user", "Mary", false, false, true, "");
-    $(By.xpath("//*[@checked='' and @name='maryremove']/..")).waitUntil(Condition.visible, Configuration.timeout);
-    info("Test 03: Delete Permission");
-    ecms_permission.deletePermissionNode("mary");
-    ecms_permission.closePermission();
-    info("Delete data test");
-    manageLogInOut.signIn(DATA_USER1, "gtngtn");
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.deleteData(node1);
-  }
-
-  @Test
-  @Tag("eabis")
-  public void test02_EditPermission() {
-    info("Test 1: Add Permission");
-    info("Get data test");
-    String node1 = "node1" + getRandomNumber();
-    String folderType = "Content Folder";
-    info("Finished getting data test");
-    navigationToolbar.goToSiteExplorer();
     siteExplorerHome.goToAddNewFolder();
     info("Add permisison for a node");
     // Create node1, node2
-    siteExplorerHome.createFolder(node1, folderType);
-    siteExplorerHome.selectNode(node1);
-    // Add permission "read", "write" for mary
-    siteExplorerHome.goToPermission();
-    ecms_permission.deletePermissionNode("mary");
-    ecms_permission.changeRight("user", "Mary", true, true, false, "");
-    ecms_permission.closePermission();
-    manageLogInOut.signIn(DATA_USER2, DATA_PASS);
-    navigationToolbar.goToSiteExplorer();
-    // Check if mary has edit, read on node1
-    siteExplorerHome.selectNode(node1);
-    // Delete data
-    manageLogInOut.signIn(DATA_USER1, "gtngtn");
-    navigationToolbar.goToSiteExplorer();
-    info("Test 02: Edit Permission");
-    siteExplorerHome.selectNode(node1);
+    siteExplorerHome.createFolder(node2, folderType);
+    info("Edit Permission");
+    siteExplorerHome.selectNode(node2);
     siteExplorerHome.goToPermission();
     ecms_permission.changeRight("user", "Mary", false, false, true, "");
-    $(By.xpath("//*[@checked='' and @name='maryremove']/..")).waitUntil(Condition.visible, Configuration.timeout);
-    info("Test 03: Delete Permission");
+    $(By.xpath("//*[@checked='' and @name='maryremove']/..")).waitUntil(Condition.visible, Configuration.openBrowserTimeoutMs);
+    info("Delete Permission");
     ecms_permission.deletePermissionNode("mary");
     ecms_permission.closePermission();
     info("Delete data test");
     manageLogInOut.signIn(DATA_USER1, "gtngtn");
     navigationToolbar.goToSiteExplorer();
     siteExplorerHome.deleteData(node1);
-  }
-
-  @Test
-  @Tag("eabis")
-  public void test_03DeletePermission() {
-    info("Test 1: Add Permission");
-    info("Get data test");
-    String node1 = "node1" + getRandomNumber();
-    String folderType = "Content Folder";
-    info("Finished getting data test");
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.goToAddNewFolder();
-    info("Add permisison for a node");
-    // Create node1, node2
-    siteExplorerHome.createFolder(node1, folderType);
-    siteExplorerHome.selectNode(node1);
-    // Add permission "read", "write" for mary
-    siteExplorerHome.goToPermission();
-    ecms_permission.deletePermissionNode("mary");
-    ecms_permission.changeRight("user", "Mary", true, true, false, "");
-    ecms_permission.closePermission();
-    manageLogInOut.signIn(DATA_USER2, DATA_PASS);
-    navigationToolbar.goToSiteExplorer();
-    // Check if mary has edit, read on node1
-    siteExplorerHome.selectNode(node1);
-    // Delete data
-    manageLogInOut.signIn(DATA_USER1, "gtngtn");
-    navigationToolbar.goToSiteExplorer();
-    info("Test 02: Edit Permission");
-    siteExplorerHome.selectNode(node1);
-    siteExplorerHome.goToPermission();
-    ecms_permission.changeRight("user", "Mary", false, false, true, "");
-    $(By.xpath("//*[@checked='' and @name='maryremove']/..")).waitUntil(Condition.visible, Configuration.timeout);
-    info("Test 03: Delete Permission");
-    ecms_permission.deletePermissionNode("mary");
-    ecms_permission.closePermission();
-    info("Delete data test");
-    manageLogInOut.signIn(DATA_USER1, "gtngtn");
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.deleteData(node1);
-  }
-
-  /**
-   * <li>Case ID:116609.</li>
-   * <li>Test Case Name: View metadata.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li> Step Number: 1 Step Name: - Step Description: Step
-   * 1: View metadata Input Data: - Upload a file - Select it - Click Metadata
-   * Expected Outcome: View matadata form is shown
-   */
-  @Test
-  @Tag("eabis")
-  public void test04_ViewMetadata() {
-    info("Test 04: View metadata");
-
+    siteExplorerHome.deleteData(node2);
+    info("View metadata");
     navigationToolbar.goToSiteExplorer();
     siteExplorerHome.goToPath("sites/intranet/documents", "Collaboration");
     $(byClassName("uiIconEcmsViewWeb")).waitUntil(Condition.visible,Configuration.timeout).click();
@@ -206,5 +89,4 @@ public class EcmsSEInfoTestIT extends Base {
     refresh();
     siteExplorerHome.deleteData("testavatar.pdf");
   }
-
 }
--- platform/src/test/java/org/exoplatform/platform/qa/ui/platform/ecms/EcmsSESearchTestIT.java
+++ platform/src/test/java/org/exoplatform/platform/qa/ui/platform/ecms/EcmsSESearchTestIT.java
@@ -1,20 +1,16 @@
 package org.exoplatform.platform.qa.ui.platform.ecms;
-
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static org.exoplatform.platform.qa.ui.selenium.Utils.getRandomNumber;
 import static org.exoplatform.platform.qa.ui.selenium.locator.ecms.ECMSLocator.*;
 import static org.exoplatform.platform.qa.ui.selenium.logger.Logger.info;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-
 import org.exoplatform.platform.qa.ui.commons.Base;
 import org.exoplatform.platform.qa.ui.core.PLFData;
 import org.exoplatform.platform.qa.ui.core.context.BugInPLF;
@@ -23,20 +19,14 @@ import org.exoplatform.platform.qa.ui.ecms.pageobject.SiteExplorerHome;
 import org.exoplatform.platform.qa.ui.selenium.ManageAlert;
 import org.exoplatform.platform.qa.ui.selenium.platform.ManageLogInOut;
 import org.exoplatform.platform.qa.ui.selenium.platform.NavigationToolbar;
-
 @Tag("ecms")
 @Tag("sniff")
 public class EcmsSESearchTestIT extends Base {
   NavigationToolbar navigationToolbar;
-
   SiteExplorerHome  siteExplorerHome;
-
   ManageLogInOut    manageLogInOut;
-
   ManageAlert       manageAlert;
-
   CreateNewDocument createNewDocument;
-
   @BeforeEach
   public void setupBeforeMethod() {
     info("Start setUpBeforeMethod");
@@ -47,98 +37,60 @@ public class EcmsSESearchTestIT extends Base {
     createNewDocument = new CreateNewDocument(this);
     manageLogInOut.signInCas(PLFData.DATA_USER1, "gtngtn");
   }
-
-  /**
-   * <li>Case ID:116566.</li>
-   * <li>Test Case Name: Advanced search.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li>
-   */
   @Test
-  public void test01_AdvancedSearch() {
-    info("Test 1: Advanced search");
-
-    String name = "name" + getRandomNumber();
-
-    navigationToolbar.goToSiteExplorer();
-    /*
-     * Step Number: 1 Step Name: Step 1: Advanced search Step Description: - Go to
-     * Sites Explorer - Click Saved Searched icon on the sidebar - Click Advanced
-     * Search icon - Choose Constraint and fill value to search, click Add - Click
-     * Search button Input Data: Expected Outcome: Search Result tab is shown with
-     * result
-     */
-    siteExplorerHome.goToAdvancedSearch();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAME).setValue(name);
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SEARCHBTN).click();
-    $(By.xpath("//*[@class='active']//*[@href='#tab-AdvancedSearchResult']")).waitUntil(Condition.visible, Configuration.timeout);
-    ELEMENT_BUTTON_CLOSE_SEARCH.click();
-  }
-
-  /**
-   * <li>Case ID:116594.</li>
-   * <li>Test Case Name: Create query in Advanced search.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li>
-   * <li>Case ID:116647.</li>
-   * <li>Test Case Name: Delete query in Advanced search.</li>
-   * <li>Pre-Condition: A query is created.</li>
-   * <li>Post-Condition:</li> Step Number: 1 Step Name: - Step Description: Step
-   * 1: Create query in Advanced search Input Data: - Go to Sites Explorer - Click
-   * Saved search icon on the sidebar - Click advanced search - Go to New query
-   * tab - Perform to create a query Expected Outcome: Query is created
-   * successfully
-   */
-  @Test
-  public void test02_CreateQueryInAdvancedSearch() {
-    info("Test 2: Create query in Advanced search");
-    String name = "name" + getRandomNumber();
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.goToAdvancedSearch();
-    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
-    $(byXpath("//*[@id='UISavedQuery']//*[text()='" + name + "']")).waitUntil(Condition.visible, Configuration.timeout);
-    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_DELETEQUERYBTN).replace("${name}", name))).click();
-    manageAlert.acceptAlert();
-    $(byXpath("//*[text()='" + name + "']")).shouldNot(Condition.visible);
-    ELEMENT_BUTTON_CLOSE_SEARCH.click();
-  }
-
-  @Test
-  public void test04_DeleteQueryInAdvancedSearch() {
-    info("Test 2: Create query in Advanced search");
-    String name = "name" + getRandomNumber();
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.goToAdvancedSearch();
-    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
-    $(byXpath("//*[@id='UISavedQuery']//*[text()='" + name + "']")).waitUntil(Condition.visible, Configuration.timeout);
-    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_DELETEQUERYBTN).replace("${name}", name))).click();
-    manageAlert.acceptAlert();
-    $(byXpath("//*[text()='" + name + "']")).shouldNot(Condition.visible);
-    ELEMENT_BUTTON_CLOSE_SEARCH.click();
-  }
-
-  /**
-   * <li>Case ID:116607.</li>
-   * <li>Test Case Name: Simple Search.</li>
-   * <li>Pre-Condition:</li>
-   * <li>Post-Condition:</li>
-   */
-  @Test
-  @Tag("eabis")
-  public void test03_SimpleSearch() {
-    info("Test 3: Simple Search");
+  public void test01_CreateEditQueryInAdvancedSearch() {
     String title = "title" + getRandomNumber();
     String content = "content" + getRandomNumber();
-    /*
-     * Step Number: 1 Step Name: - Step Description: Step 1: Simple Search Input
-     * Data: - Go to Content Explorer - Input keyword into Search field above action
-     * bar - Click Quick Search or press Enter Expected Outcome: The result appears:
-     * all documents which has title or content including search keyword appear
-     */
+    info("Create query in Advanced search");
+    String name = "name" + getRandomNumber();
+    String name1 = "name" + getRandomNumber();
+    info("Set up browsing Preferences");
+    navigationToolbar.goToSiteExplorer();
+    siteExplorerHome.openSettingsDriver(SiteExplorerHome.selectDriverOption.MODIFIEDDATE,
+            SiteExplorerHome.selectDriverOrder.DESCENDING);
+    $(By.xpath("//*[@class='nodeLabel']//*[text()='intranet']")).waitUntil(Condition.visible, Configuration.timeout);
+    siteExplorerHome.openSettingsDriver(SiteExplorerHome.selectDriverOption.ALPHABETICAL,
+            SiteExplorerHome.selectDriverOrder.ASCENDING);
+    navigationToolbar.goToSiteExplorer();
+    siteExplorerHome.goToAdvancedSearch();
+    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
+    $(byXpath("//*[@id='UISavedQuery']//*[text()='" + name + "']")).waitUntil(Condition.visible, Configuration.timeout);
+    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_DELETEQUERYBTN).replace("${name}", name))).click();
+    manageAlert.acceptAlert();
+    $(byXpath("//*[text()='" + name + "']")).shouldNot(Condition.visible);
+    ELEMENT_BUTTON_CLOSE_SEARCH.click();
+    sleep(2000);
+    info("Execute query in Advanced search");
+    navigationToolbar.goToSiteExplorer();
+    siteExplorerHome.goToAdvancedSearch();
+    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
+    $(byXpath("//*[text()='" + name + "']")).waitUntil(Condition.visible, Configuration.timeout);
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_EXECUTEQUERYBTN).click();
+    $(byXpath("//*[@class='active']//*[@href='#tab-AdvancedSearchResult']")).waitUntil(Condition.visible, Configuration.timeout);
+    ELEMENT_BUTTON_CLOSE_SEARCH.click();
+    info("Edit query in Advanced search");
+    sleep(1000);
+    navigationToolbar.goToSiteExplorer();
+    siteExplorerHome.goToAdvancedSearch();
+    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name1);
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
+    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT.replace("${name}", name1))).waitUntil(Condition.visible,
+            Configuration.timeout);
+    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_EDITQUERYBTN).replace("${name}", name1))).click();
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_QUERYTYPE).selectOption("xPath");
+    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEEDITQUERYBTN).click();
+    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT1.replace("${name}", name1))).waitUntil(Condition.visible,
+            Configuration.timeout);
+    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_DELETEQUERYBTN).replace("${name}", name1))).click();
+    manageAlert.acceptAlert();
+    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT.replace("${name}", name1))).shouldNot(Condition.visible);
+    ELEMENT_BUTTON_CLOSE_SEARCH.click();
+    info("Simple Search");
     navigationToolbar.goToSiteExplorer();
     siteExplorerHome.goToPath("intranet/documents", "Site Management");
     siteExplorerHome.goToAddNewContent();
@@ -148,68 +100,10 @@ public class EcmsSESearchTestIT extends Base {
     siteExplorerHome.goToPath("intranet/documents", "Site Management");
     sleep(Configuration.openBrowserTimeoutMs);
     $(ELEMENT_ACTIONBAR_SEARCHBAR).setValue(title).waitUntil(Condition.visible,Configuration.openBrowserTimeoutMs).pressEnter();
-    sleep(Configuration.timeout);
+    sleep(2000);
     $(byId("SimpleSearchResult")).find(byText(title)).waitUntil(Condition.visible, Configuration.openBrowserTimeoutMs);
     navigationToolbar.goToSiteExplorer();
     siteExplorerHome.goToPath("intranet/documents", "Site Management");
     siteExplorerHome.deleteData(title);
-  }
-
-  /**
-   * <li>Case ID:116648.</li>
-   * <li>Test Case Name: Execute query in Advanced search.</li>
-   * <li>Pre-Condition: A query is created.</li>
-   * <li>Post-Condition:</li> Step Number: 1 Step Name: - Step Description: Step
-   * 1: Execute query in Advanced search Input Data: - Go to Sites Explorer -
-   * Click Saved search icon on the sidebar - Click advanced search - Go to Saved
-   * Query tab - Click Execute icon of a saved query Expected Outcome: Query is
-   * executed successfully
-   */
-  @Test
-  public void test05_ExecuteQueryInAdvancedSearch() {
-    info("Test 5: Execute query in Advanced search");
-    String name = "name" + getRandomNumber();
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.goToAdvancedSearch();
-    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
-    $(byXpath("//*[text()='" + name + "']")).waitUntil(Condition.visible, Configuration.timeout);
-
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_EXECUTEQUERYBTN).click();
-    $(byXpath("//*[@class='active']//*[@href='#tab-AdvancedSearchResult']")).waitUntil(Condition.visible, Configuration.timeout);
-    ELEMENT_BUTTON_CLOSE_SEARCH.click();
-  }
-
-  /**
-   * <li>Case ID:116649.</li>
-   * <li>Test Case Name: Edit query in Advanced search.</li>
-   * <li>Pre-Condition: A query is created.</li>
-   * <li>Post-Condition:</li> Step Number: 1 Step Name: - Step Description: Step
-   * 1: Edit query in Advanced search Input Data: - Go to Sites Explorer - Click
-   * Saved search icon on the sidebar - Click advanced search - Go to Saved Query
-   * tab - Click on corresponding Edit icon of a query - Perform to edit the query
-   * Expected Outcome: Query is edited successfully
-   */
-  @Test
-  public void test06_EditQueryInAdvancedSearch() {
-    info("Test 6: Edit query in Advanced search");
-    String name = "name" + getRandomNumber();
-    navigationToolbar.goToSiteExplorer();
-    siteExplorerHome.goToAdvancedSearch();
-    $(ELEMENT_SITEXPLORER_ADVANCEDSEARCH_CREATEQUERYTAB).click();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_NAMEQUERY).setValue(name);
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEQUERYBTN).click();
-    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT.replace("${name}", name))).waitUntil(Condition.visible,
-                                                                                              Configuration.timeout);
-    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_EDITQUERYBTN).replace("${name}", name))).click();
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_QUERYTYPE).selectOption("xPath");
-    $(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_SAVEEDITQUERYBTN).click();
-    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT1.replace("${name}", name))).waitUntil(Condition.visible,
-                                                                                               Configuration.timeout);
-    $(byXpath((ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_DELETEQUERYBTN).replace("${name}", name))).click();
-    manageAlert.acceptAlert();
-    $(byXpath(ELEMENT_SITEEXPLORER_ADVANCEDSEARCH_RESULT.replace("${name}", name))).shouldNot(Condition.visible);
-    ELEMENT_BUTTON_CLOSE_SEARCH.click();
   }
 }
